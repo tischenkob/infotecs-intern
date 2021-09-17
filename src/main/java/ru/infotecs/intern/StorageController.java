5ef593570c378b6db7223997bd4a7270987f52a5
Bogdan Tischenko <tischenkob@gmail.com>
2021-09-17 17:50:20 +0300

    Implement file upload

--- src/main/java/ru/infotecs/intern/StorageController.java
+++ src/main/java/ru/infotecs/intern/StorageController.java
@@ -3,17 +3,16 @@ package ru.infotecs.intern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.FileSystemResource;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.time.LocalDateTime;
-import java.util.Properties;
-import java.util.stream.Collectors;
 
 import static ru.infotecs.intern.StorageController.Response.Status;
 
@@ -22,8 +21,10 @@ import static ru.infotecs.intern.StorageController.Response.Status;
 @RequiredArgsConstructor
 public class StorageController {
 
-  private final TimedStorage<String> storage;
-  private final String               dumpFileName = "storage_dump.properties";
+  private final StorageService service;
+  @Setter
+  @Value("${dump.file.name:storage_dump.properties}")
+  private String dumpFileName;
 
   @PostMapping("/records/{key}")
   public Response createRecord(
@@ -31,42 +32,48 @@ public class StorageController {
       @RequestParam("value") String value,
       @RequestParam(value = "ttl", required = false) Integer ttl
                               ) {
-    if (ttl != null && ttl < 0) {
+    if (ttl != null && ttl < 1) {
       return new Response(Status.FAILURE, "ttl must be greater than 0");
     }
-
-    if (ttl == null) {
-      storage.put(key, value);
-    } else {
-      storage.put(key, value, ttl);
-    }
+    service.createRecord(key, value, ttl);
     return new Response(Status.SUCCESS, "New record was created!");
   }
 
   @GetMapping("/records/{key}")
   public Response readRecord(@PathVariable String key) {
-    return storage.get(key).map(value -> new Response(Status.SUCCESS, "Record found.", value))
+    return service.readRecord(key)
+                  .map(value -> new Response(Status.SUCCESS, "Record found.", value))
                   .orElseGet(() -> new Response(Status.FAILURE, "Record not found."));
   }
 
   @DeleteMapping("/records/{key}")
   public Response deleteRecord(@PathVariable String key) {
-    return storage.remove(key).isPresent() ? new Response(Status.SUCCESS, "Successfully removed.")
-                                           : new Response(Status.FAILURE, "Record not Found.");
+    return service.deleteRecord(key)
+                  .isPresent() ? new Response(Status.SUCCESS, "Successfully removed.")
+                               : new Response(Status.FAILURE, "Record not Found.");
   }
 
-  @GetMapping(value = "dump", produces = MediaType.TEXT_PLAIN_VALUE)
-  public FileSystemResource getDump() {
-    var stringsMap = storage.getInnerMap().entrySet().stream().collect(Collectors.toMap(
-        e -> e.getKey().toString(), e -> e.getValue().toString()));
-    Properties properties = new Properties();
-    properties.putAll(stringsMap);
-    try (OutputStream stream = new FileOutputStream(dumpFileName)) {
-      properties.store(stream, LocalDateTime.now().toString());
-      stream.flush();
-      return new FileSystemResource(new File(dumpFileName));
+  @ResponseBody
+  @GetMapping(value = "dump", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+  public ResponseEntity<File> getDump() throws IOException {
+    File file = service.getDumpFile();
+    var response = ResponseEntity.ok()
+                                 .header(HttpHeaders.CONTENT_DISPOSITION,
+                                         "attachment; filename=\"" + file.getName() + "\"")
+                                 .body(file);
+    if (!file.delete()) {
+      throw new IOException("Temporary dump file %s could not be deleted".formatted(file.getName()));
+    }
+    return response;
+  }
+
+  @PostMapping("load")
+  public Response loadDump(@RequestParam("file") MultipartFile file) {
+    try {
+      service.loadDump(file.getInputStream());
+      return new Response(Status.SUCCESS, "File was successfully loaded.");
     } catch (IOException e) {
-      throw new RuntimeException(e);
+      return new Response(Status.FAILURE, "File could not be loaded.");
     }
   }
 
@@ -76,7 +83,7 @@ public class StorageController {
   static class Response {
     private final Status status;
     private final String message;
-    private       Object data;
+    private Object data;
 
     enum Status {
       SUCCESS, FAILURE
---
+++ src/main/java/ru/infotecs/intern/StorageService.java
@@ -0,0 +1,63 @@
+package ru.infotecs.intern;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Properties;
+
+import static java.util.stream.Collectors.toMap;
+
+
+@RequiredArgsConstructor
+public @Service
+class StorageService {
+  private final TimedStorage storage;
+
+  public void createRecord(String key, String value, @Nullable Integer ttl) {
+    if (ttl == null) {
+      storage.put(key, value);
+    } else {
+      storage.put(key, value, ttl);
+    }
+  }
+
+
+  public Optional<Object> readRecord(String key) {
+    return storage.get(key);
+  }
+
+  public Optional<Object> deleteRecord(String key) {
+    return storage.remove(key);
+  }
+
+  public File getDumpFile() throws IOException {
+    Map<String, String> stringsMap = storage.getInnerMap()
+                                            .entrySet()
+                                            .stream()
+                                            .collect(toMap(
+                                                e -> e.getKey().toString(),
+                                                e -> e.getValue().toString()));
+    Properties properties = new Properties();
+    properties.putAll(stringsMap);
+    File dumpFile = File.createTempFile("dump", "infotecs");
+    try (OutputStream stream = new FileOutputStream(dumpFile)) {
+      properties.store(stream, LocalDateTime.now().toString());
+      stream.flush();
+      return dumpFile;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void loadDump(InputStream file) throws IOException {
+    Properties properties = new Properties();
+    properties.load(file);
+    storage.clear();
+    storage.load(properties);
+  }
+}
